ac29b5af7b58fd7e30f808f813bfb386b567acf4
maria <mariataktasheva@gmail.com>
2024-01-16 16:30:47 +0000

    Added antialiasing

--- rtweekend.h
+++ rtweekend.h
@@ -2,6 +2,7 @@
 #define RTWEEKEND_H
 
 #include <cmath>
+#include <cstdlib>
 #include <limits>
 #include <memory>
 
@@ -14,10 +15,14 @@ const double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(double degrees) { return degrees * pi / 180.0; }
 
-// Common Headers
+inline double random_double() {
+    // Returns a random real in [0,1).
+    return rand() / (RAND_MAX + 1.0);
+}
 
-#include "interval.h"
-#include "ray.h"
-#include "vec3.h"
+inline double random_double(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max - min) * random_double();
+}
 
 #endif
