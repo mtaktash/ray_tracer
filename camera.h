e40befdf6c40a21fbca8407ee489c2b77f6bbb55
maria <mariataktasheva@gmail.com>
2024-01-16 18:36:51 +0000

    Msx depth for ray reflection and gamma correction

--- camera.h
+++ camera.h
@@ -13,6 +13,7 @@ class camera {
     double aspect_ratio = 1.0;   // Ratio of image width over height
     int image_width = 100;       // Rendered image width in pixel count
     int samples_per_pixel = 10;  // Count of random samples for each pixel
+    int max_depth = 10;          // Maximum number of ray bounces into scene
 
     void render(const hittable& world) {
         initialize();
@@ -25,7 +26,7 @@ class camera {
                 color pixel_color(0, 0, 0);
                 for (int sample = 0; sample < samples_per_pixel; ++sample) {
                     ray r = get_ray(i, j);
-                    pixel_color += ray_color(r, world);
+                    pixel_color += ray_color(r, max_depth, world);
                 }
                 write_color(std::cout, pixel_color, samples_per_pixel);
             }
@@ -67,12 +68,15 @@ class camera {
         pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
     }
 
-    color ray_color(const ray& r, const hittable& world) const {
+    color ray_color(const ray& r, int depth, const hittable& world) const {
         hit_record rec;
 
-        if (world.hit(r, interval(0, infinity), rec)) {
-            vec3 direction = random_on_hemisphere(rec.normal);
-            return 0.5 * ray_color(ray(rec.p, direction), world);
+        // If we've exceeded the ray bounce limit, no more light is gathered.
+        if (depth <= 0) return color(0, 0, 0);
+
+        if (world.hit(r, interval(0.0001, infinity), rec)) {
+            vec3 direction = rec.normal + random_unit_vector();
+            return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
