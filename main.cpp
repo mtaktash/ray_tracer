780a08ff9506ffb6f97772420594f99c0526c6a7
maria <mariataktasheva@gmail.com>
2024-01-21 19:13:49 +0000

    Add texture image and test earth texture

--- main.cpp
+++ main.cpp
@@ -97,5 +97,38 @@ void two_spheres() {
 
     cam.render(world);
 }
+void earth() {
+    auto earth_texture = std::make_shared<image_texture>("earthmap.jpg");
+    auto earth_surface = std::make_shared<lambertian>(earth_texture);
+    auto globe = std::make_shared<sphere>(point3(0, 0, 0), 2, earth_surface);
 
-int main() { random_spheres(); }
+    camera cam;
+
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
+    cam.samples_per_pixel = 100;
+    cam.max_depth = 50;
+
+    cam.vfov = 20;
+    cam.lookfrom = point3(0, 0, 12);
+    cam.lookat = point3(0, 0, 0);
+    cam.vup = vec3(0, 1, 0);
+
+    cam.defocus_angle = 0;
+
+    cam.render(hittable_list(globe));
+}
+
+int main() {
+    switch (3) {
+        case 1:
+            random_spheres();
+            break;
+        case 2:
+            two_spheres();
+            break;
+        case 3:
+            earth();
+            break;
+    }
+}
--- texture.h
+++ texture.h
@@ -2,6 +2,7 @@
 #define TEXTURE_H
 
 #include "color.h"
+#include "rtw_stb_image.h"
 #include "rtweekend.h"
 
 class texture {
@@ -45,6 +46,30 @@ class checker_texture : public texture {
     double inv_scale;
     std::shared_ptr<texture> even;
     std::shared_ptr<texture> odd;
+};
+
+class image_texture : public texture {
+   public:
+    image_texture(const char* filename) : image(filename) {}
+
+    color value(double u, double v, const point3& p) const override {
+        // If we have no texture data, then return solid cyan as a debugging aid.
+        if (image.height() <= 0) return color(0, 1, 1);
+
+        // Clamp input texture coordinates to [0,1] x [1,0]
+        u = interval(0, 1).clamp(u);
+        v = 1.0 - interval(0, 1).clamp(v);  // Flip V to image coordinates
+
+        auto i = static_cast<int>(u * image.width());
+        auto j = static_cast<int>(v * image.height());
+        auto pixel = image.pixel_data(i, j);
+
+        auto color_scale = 1.0 / 255.0;
+        return color(color_scale * pixel[0], color_scale * pixel[1], color_scale * pixel[2]);
+    }
+
+   private:
+    rtw_image image;
 };
 
 #endif
