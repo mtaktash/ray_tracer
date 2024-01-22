d35cb7fa248ad8840ded93850eba99330039a56e
maria <mariataktasheva@gmail.com>
2024-01-22 20:02:18 +0000

    Vector instead of double for better perlin noise

--- texture.h
+++ texture.h
@@ -77,7 +77,9 @@ class noise_texture : public texture {
    public:
     noise_texture(double sc) : scale(sc) {}
 
-    color value(double u, double v, const point3& p) const override { return color(1, 1, 1) * noise.noise(scale * p); }
+    color value(double u, double v, const point3& p) const override {
+        return color(1, 1, 1) * 0.5 * (1.0 + noise.noise(scale * p));
+    }
 
    private:
     perlin noise;
