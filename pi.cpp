992a7393161b00ffe930b9f3611e3e3b1ec5edb8
maria <mariataktasheva@gmail.com>
2024-01-30 15:28:55 +0000

    Stratified sampling of pi

--- pi.cpp
+++ pi.cpp
@@ -8,14 +8,20 @@
 
 int main() {
     int inside_circle = 0;
-    int runs = 0;
-    std::cout << std::fixed << std::setprecision(12);
-    while (true) {
-        runs++;
-        auto x = random_double(-1, 1);
-        auto y = random_double(-1, 1);
-        if (x * x + y * y < 1) inside_circle++;
-
-        if (runs % 10000000 == 0) std::cout << "Estimate of Pi = " << (4.0 * inside_circle) / runs << '\n';
+    int inside_circle_stratified = 0;
+    int sqrt_N = 1000;
+    for (int i = 0; i < sqrt_N; i++) {
+        for (int j = 0; j < sqrt_N; j++) {
+            auto x = random_double(-1, 1);
+            auto y = random_double(-1, 1);
+            if (x * x + y * y < 1) inside_circle++;
+            x = 2 * ((i + random_double()) / sqrt_N) - 1;
+            y = 2 * ((j + random_double()) / sqrt_N) - 1;
+            if (x * x + y * y < 1) inside_circle_stratified++;
+        }
     }
+
+    std::cout << std::fixed << std::setprecision(12);
+    std::cout << "Regular    Estimate of Pi = " << (4.0 * inside_circle) / (sqrt_N * sqrt_N) << '\n'
+              << "Stratified Estimate of Pi = " << (4.0 * inside_circle_stratified) / (sqrt_N * sqrt_N) << '\n';
 }
